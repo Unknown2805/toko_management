26a892c382a737125805b8b28942c0b0ae1874a8
Unknown2805 <nuraliali2805@gmail.com>
2022-08-24 15:04:50 +0700

    first commit

---
+++ app/Models/Supplier.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    use HasFactory;
+
+    protected $table = 'suppliers';
+
+    protected $fillable = [
+        'name',
+        'address',
+        'email',
+        'product_id',
+    ];
+
+    public function products(){
+        $this->hasMany(Product::class);
+    }
+}
