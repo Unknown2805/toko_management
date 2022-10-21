a465593963d37fe0250b93ed4db94e72f68a21b1
Unknown2805 <nuraliali2805@gmail.com>
2022-10-22 01:37:48 +0700

    dashboard preview

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -59,7 +59,7 @@
                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                 <h6 class="text-muted font-semibold">Stock Product</h6>
                                 
-                                <h6 class="font-extrabold mb-0">{{$stock}}</h6>
+                                <h6 class="font-extrabold mb-0">{{$stock}} products</h6>
                             </div>
                         </div>
                     </div>
@@ -70,14 +70,14 @@
                     <div class="card-body px-4 py-4-5">
                         <div class="row">
                             <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                <div class="stats-icon blue mb-2">
-                                    <i class="ibi bi-box"></i>
+                                <div class="stats-icon red mb-2">
+                                    <i class="bi-box-arrow-in-up-right"></i>
                                 </div>
                             </div>
                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                 <h6 class="text-muted font-semibold">Sold Product</h6>
                                 
-                                <h6 class="font-extrabold mb-0">{{$sold}}</h6>
+                                <h6 class="font-extrabold mb-0">{{$sold}} products</h6>
                             </div>
                         </div>
                     </div>
